827b3b145e0cdd15d28ea3ab6ddcb5d145a3df04
Vu Quoc Ngoc <vuquocngoc1989@gmail.com>
2022-10-22 22:11:05 +0700

    redux-toolkit-22/10/22

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 // import { connect } from "react-redux";
 // import {
 //   increaseCounter,
@@ -10,49 +10,28 @@
 // import {useEffect} from 'react';
 // import axios from 'axios';
 import Home from './components/Home';
+import { useDispatch, useSelector } from 'react-redux';
+import {increment, decrement} from './redux/slices/counterSlice';
+
 function App(props) {
-
-  // const newCount = useSelector(
-  //   (state) => {return state.counter.count}
-  //   ); 
-  //   const dispatch = useDispatch(); 
-
-  // const handleIncrease = () => {
-  //   // dispatch action
-  //   // props.increaseCounter1();
-  //   dispatch(increaseCounter()); 
-  // }
-
-  // const fetchAllUser = async() => {
-  //   const res = await axios.get('http://localhost:8080/users/all'); 
-  //   console.log('>>> res data: ', res);
-  // }
-
-  // useEffect(() => {
-  //   fetchAllUser();
-  // }, []);
-  
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.counter.value)
   return (
-    <>
-      <Home/>
-    </>
+    <div className='App' >
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Hello world with React Redux-toolkit</h1>
+
+        <div className='btn-actions'>
+          <button onClick={()=>dispatch(increment())} >Decrease</button>
+          <button onClick={()=>dispatch(decrement())} >Increase</button>
+
+        </div>
+        <br/>
+        <div>Count = {count}</div>
+      </header>
+    </div>
   );
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     count: state.counter.count,
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     increaseCounter1: () => dispatch(increaseCounter()),
-
-//     decreaseCounter: () => dispatch(decreaseCounter()),
-//   }
-// }
-
-// connect as HOC: Higher Order Component
-// export default connect(mapStateToProps, mapDispatchToProps)(App)
 export default App;
