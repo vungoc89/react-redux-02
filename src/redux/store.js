0aaa0b92824ca3294a86233ec942a0b6b3fc1171
Vu Quoc Ngoc <vuquocngoc1989@gmail.com>
2022-10-31 09:21:55 +0700

    31/10/22

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slices/counterSlice'; 
+import counterReducer from './slices/counterSlice'; //trong file counterSlice.js ko co counterReducer thi lay o dau 
 
 const store = configureStore({
     reducer: {
